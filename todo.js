ebe25e29e3ef74ad8767ebcd7bac7f041a481faf
Kyle Butler <kylbutlr@gmail.com>
2018-11-13 16:30:53 -0800

    reverting to pre-express

--- todo.js
+++ todo.js
@@ -1,17 +1,20 @@
 /* jshint asi: true */
 /* jshint esversion: 6 */
 
+const http = require('http')
 const fs = require('fs')
-const express = require('express')
-const todo = express()
+//const express = require('express')
+//const todo = express()
 
-todo.use('/css', express.static('css'))
+//todo.use('/css', express.static('css'))
 
-todo.get('/', function(req, res){
+//todo.get('/', function(req, res){
+const server = http.createServer((req, res) => {
   console.log('Request was made: '+ req.url)
   res.writeHead(200, {'Content-Type': 'text/html'})
   fs.createReadStream(__dirname + '/public/index.html').pipe(res)
 })
 
-todo.listen(3000)
+server.listen(3000)
+//todo.listen(3000)
 console.log("Listening on post 3000")
