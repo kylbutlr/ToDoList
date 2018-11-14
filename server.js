57fbf6003e1181940a66790a30ac3297c31fadd9
Kyle Butler <kylbutlr@gmail.com>
2018-11-13 22:02:49 -0800

    minor change

--- server.js
+++ server.js
@@ -8,20 +8,20 @@ const todos = []
 
 //todo.get('/', function(req, res){
 const server = http.createServer((req, res) => {
-  console.log('Request was made: '+ req.url)
   res.setHeader("Access-Control-Allow-Origin", "*")
-  if (req.url === '/todos') {
+  console.log('Request was made: '+ req.url)
+  if (req.method === 'GET') {
     res.end(JSON.stringify({ todos }));
   }
   else if (req.method === 'POST'){
-    let body = '';
+    let body = ''
     req.on('data', chunk => {
       body += chunk.toString(); // convert Buffer to string
-    });
+    })
     req.on('end', () => {
-      console.log(body);
-      res.end('ok');
-    });
+      console.log(body)
+      res.end('ok')
+    })
   } 
   else {
     res.end('404: Not Found');
