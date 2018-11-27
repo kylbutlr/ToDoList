6545b21c9f17bdba68803ae199e5cdaea00de222
Kyle Butler <kylbutlr@gmail.com>
2018-11-26 21:40:55 -0800

    updated WIP

--- test.js
+++ test.js
@@ -1,9 +1,18 @@
 const app = require('./app')
+const request = require('supertest')
 
 describe('GET /todos', function() {
   it('respond with json', function (done) {
     request(app)
       .get('/todos')
       .expect(200, done)
+  })
+})
+
+describe('POST /todos', function() {
+  it('respond with json', function (done) {
+    request(app)
+      .post('/todos')
+      .expect(201, done)
   })
 })
