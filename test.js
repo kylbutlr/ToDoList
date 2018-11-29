d95ec46016614a34c376d6ddc69be3a3326bf3af
Kyle Butler <kylbutlr@gmail.com>
2018-11-28 17:53:56 -0800

    WIP

--- test.js
+++ test.js
@@ -1,5 +1,13 @@
+const fs = require('fs')
 const request = require('supertest')
 const app = require('./app')
+
+afterAll(() => {
+  fs.writeFile('todos.json', JSON.stringify({ "nextKey": 2, "todos": [
+    {"text": "Delete this item", "time24": "", "date": "2018-11-15", "done": "true", "key": 0},
+    {"text": "Add more to my list", "time24": "", "date": "2018-11-15", "done":"false", "key": 1}
+  ]}, null, 2))
+})
 
 describe('GET /todos', function() {
   it('respond with json', function (done) {
@@ -10,7 +18,7 @@ describe('GET /todos', function() {
 })
 
 describe('POST /todos', function() {
-  it('respond with json', function (done) {
+  it('post with json', function (done) {
     request(app)
       .post('/todos')
       .expect(201, done)
