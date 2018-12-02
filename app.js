d1f2dab4dca3b330288435ce72c9ed8a8524833f
Kyle Butler <kylbutlr@gmail.com>
2018-12-01 17:05:32 -0800

    Moved logic out of conditional

--- app.js
+++ app.js
@@ -16,102 +16,27 @@ const server = http.createServer((req, res) => {
   console.log('Request: '+ req.url)
 
   if (req.method === 'GET' && req.url === '/todos') {
-    fs.readFile('todos.json', 'utf-8', (err,data) => {
-      if (err) { throw err }
-      res.statusCode = 200
-      res.end(data)
-    })
+    getAllTodos(res)
   }
 
   else if (req.method === 'GET' && /\/todos\/[0-9]+/.test(req.url)) {
-    const key = Number(req.url.match(/[0-9]+$/)[0])
-    fs.readFile('todos.json', 'utf-8', (err,data) => {
-      if (err) { throw err }
-      parsedData = JSON.parse(data)
-      res.statusCode = 200
-      res.end(JSON.stringify({ 
-        todo: parsedData.todos.find(t => t.key === key)
-      }))
-    })
+    getOneTodo(req,res)
   }
 
   else if (req.method === 'POST' && req.url === '/todos'){
-    let body = ''
-    req.on('data', chunk => {
-      body += chunk.toString()
-    })
-    req.on('end', () => {
-      fs.readFile('todos.json', 'utf8', (err,data) => {
-        if (err) { throw err }
-        const todosData = JSON.parse(data)
-        console.log(body)
-        const todo = querystring.parse(body)
-        console.log(todo)
-        todo.key = nextKey
-        nextKey++
-        todosData.todos.push(todo)
-        todos = todosData.todos
-        const newData = JSON.stringify({ 
-          "nextKey": nextKey, 
-          "todos": todosData.todos
-        }, null, 2)
-        fs.writeFile('todos.json', newData, (err) => {
-          if (err) { throw err }
-          res.statusCode = 201
-          res.end('POST')
-        })
-      })
-    })
+    postTodo(req,res)
   } 
 
   else if (req.method === 'PUT' && req.url === '/todos'){
-    let body = ''
-    req.on('data', chunk => {
-      body += chunk.toString()
-    })
-    req.on('end', () => {
-      const parsedBody = JSON.parse(body)
-      parsedBody.key = parseInt(parsedBody.key)
-      const t = todos.findIndex(x => x.key == parsedBody.key)
-      todos[t] = parsedBody
-      const newData = JSON.stringify({ 
-        "nextKey": nextKey, 
-        "todos": todos
-      }, null, 2)
-      fs.writeFile('todos.json', newData, (err) => {
-        if (err) { throw err }
-        res.statusCode = 204
-        res.end('PUT')
-      })
-    })
+    editTodo(req,res)
   }
 
   else if (req.method === 'DELETE' && req.url === '/todos'){
-    todos.splice(0,todos.length)
-    const newData = JSON.stringify({
-      "nextKey": nextKey, 
-      "todos": []
-    }, null, 2)
-    fs.writeFile('todos.json', newData, (err) => {
-      if (err) { throw err }
-      res.statusCode = 204
-      res.end('CLEAR')
-    })
+    deleteAllTodos(req,res)
   }
 
   else if (req.method === 'DELETE' && /\/todos\/[0-9]+/.test(req.url)){
-    const key = Number(req.url.match(/[0-9]+$/)[0])
-    const t = todos.findIndex(x => x.key == key)
-    todos.splice(t,1)
-    const newData = JSON.stringify({ 
-      "nextKey": nextKey, 
-      "todos": todos
-    }, null, 2)
-    fs.writeFile('todos.json', newData, (err) => {
-      if (err) { throw err }
-      res.statusCode = 204
-      res.end('DELETE')
-    })
+    deleteOneTodo(req,res)
   }
 
   else {
@@ -119,6 +44,105 @@ const server = http.createServer((req, res) => {
     res.end('404: Not Found')
   }
 })
+
+const getAllTodos = (res) => {
+  fs.readFile('todos.json', 'utf-8', (err,data) => {
+    if (err) { throw err }
+    res.statusCode = 200
+    res.end(data)
+  })
+}
+
+const getOneTodo = (req,res) => {
+  const key = Number(req.url.match(/[0-9]+$/)[0])
+  fs.readFile('todos.json', 'utf-8', (err,data) => {
+    if (err) { throw err }
+    parsedData = JSON.parse(data)
+    res.statusCode = 200
+    res.end(JSON.stringify({ 
+      todo: parsedData.todos.find(t => t.key === key)
+    }))
+  })
+}
+
+const postTodo = (req,res) => {
+  let body = ''
+  req.on('data', chunk => {
+    body += chunk.toString()
+  })
+  req.on('end', () => {
+    fs.readFile('todos.json', 'utf8', (err,data) => {
+      if (err) { throw err }
+      const todosData = JSON.parse(data)
+      console.log(body)
+      const todo = querystring.parse(body)
+      console.log(todo)
+      todo.key = nextKey
+      nextKey++
+      todosData.todos.push(todo)
+      todos = todosData.todos
+      const newData = JSON.stringify({ 
+        "nextKey": nextKey, 
+        "todos": todosData.todos
+      }, null, 2)
+      fs.writeFile('todos.json', newData, (err) => {
+        if (err) { throw err }
+        res.statusCode = 201
+        res.end('POST')
+      })
+    })
+  })
+}
+
+const editTodo = (req,res) => {
+  let body = ''
+  req.on('data', chunk => {
+    body += chunk.toString()
+  })
+  req.on('end', () => {
+    const parsedBody = JSON.parse(body)
+    parsedBody.key = parseInt(parsedBody.key)
+    const t = todos.findIndex(x => x.key == parsedBody.key)
+    todos[t] = parsedBody
+    const newData = JSON.stringify({ 
+      "nextKey": nextKey, 
+      "todos": todos
+    }, null, 2)
+    fs.writeFile('todos.json', newData, (err) => {
+      if (err) { throw err }
+      res.statusCode = 204
+      res.end('PUT')
+    })
+  })
+}
+
+const deleteAllTodos = (req,res) => {
+  todos.splice(0,todos.length)
+  const newData = JSON.stringify({
+    "nextKey": nextKey, 
+    "todos": []
+  }, null, 2)
+  fs.writeFile('todos.json', newData, (err) => {
+    if (err) { throw err }
+    res.statusCode = 204
+    res.end('CLEAR')
+  })
+}
+
+const deleteOneTodo = (req,res) => {
+  const key = Number(req.url.match(/[0-9]+$/)[0])
+  const t = todos.findIndex(x => x.key == key)
+  todos.splice(t,1)
+  const newData = JSON.stringify({ 
+    "nextKey": nextKey, 
+    "todos": todos
+  }, null, 2)
+  fs.writeFile('todos.json', newData, (err) => {
+    if (err) { throw err }
+    res.statusCode = 204
+    res.end('DELETE')
+  })
+}
 
 fs.readFile('todos.json', 'utf-8', (err,data) => {
   if (err) { throw err }
