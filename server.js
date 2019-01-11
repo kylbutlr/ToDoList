4e3fca18d8568e3eb9896caffab63b6a15b940e7
Kyle Butler <kylbutlr@gmail.com>
2019-01-10 19:53:31 -0800

    WIP

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+const {Client} = require('pg');
 const App = require('./app');
-const client = new Client({
-    user: 'postgres',
-    password: 'pass',
-    database: 'todos'
-  });
-const app = App(client);
+const DB = require('./db');
+let app;
+let db;
+let client;
+client = new Client({
+  user: 'postgres',
+  password: 'pass',
+  database: 'todos'
+});
+client.connect();
+db = DB(client);
+app = App(client);
 app.listen(3000);
