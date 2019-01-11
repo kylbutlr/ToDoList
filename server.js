6c402042549ba69a766bb0c39ed6d6c62faaee5d
Kyle Butler <kylbutlr@gmail.com>
2019-01-10 20:10:52 -0800

    Minor change

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const {Client} = require('pg');
 const App = require('./app');
-const DB = require('./db');
 const client = new Client({
   user: 'postgres',
   password: 'pass',
   database: 'todos'
 });
 client.connect();
-const db = DB(client);
 const app = App(client);
 app.listen(3000);
