f6395d9c434014ee315dd012b1c9f80f94049301
Kyle Butler <kylbutlr@gmail.com>
2019-03-18 08:57:27 -0700

    heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const App = require('./app');
 const client = new Client({
   user: 'rkrtjfhetbrpso',
   password: '6995b828e011276cc49322a5740b10da2a87c95ff2c5055eece0154e6b5e3835',
-  database: 'dars6i8a5faoqk'
+  database: 'dars6i8a5faoqk?ssl=true'
 });
 client.connect();
 const app = App(client);
