cfd7d83c2e0225239ab8cf7fdd6f85a0d87f8deb
Kyle Butler <kylbutlr@gmail.com>
2019-03-18 09:07:31 -0700

    heroku

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const { Client } = require('pg');
 const App = require('./app');
 const client = new Client({
+  host: 'ec2-23-23-241-119.compute-1.amazonaws.com',
   user: 'rkrtjfhetbrpso',
   password: '6995b828e011276cc49322a5740b10da2a87c95ff2c5055eece0154e6b5e3835',
   port: '5432',
   database: 'dars6i8a5faoqk',
-  ssl: 'true',
+  ssl: true,
   connectionString:
     'postgres://rkrtjfhetbrpso:6995b828e011276cc49322a5740b10da2a87c95ff2c5055eece0154e6b5e3835@ec2-23-23-241-119.compute-1.amazonaws.com:5432/dars6i8a5faoqk',
 });
