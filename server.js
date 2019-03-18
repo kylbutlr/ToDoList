4ff4553170f47cefe20807a48a1be9b869d2ae29
Kyle Butler <kylbutlr@gmail.com>
2019-03-18 15:04:51 -0700

    revert

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const { Client } = require('pg');
 const App = require('./app');
 const client = new Client({
-  user: 'bsndzkjhbniysd',
-  password: 'b0e85b1ed413e75f5d8447542dfb2eaf67b1563cb26f8d71746045cea205222f',
-  database: 'dfq3ajdeehcpp4',
+  user: 'rkrtjfhetbrpso',
+  password: '6995b828e011276cc49322a5740b10da2a87c95ff2c5055eece0154e6b5e3835',
+  database: 'dars6i8a5faoqk',
   port: 5432,
-  host: 'ec2-50-17-231-192.compute-1.amazonaws.com',
+  host: 'ec2-23-23-241-119.compute-1.amazonaws.com',
   ssl: true
 });
 client.connect();
