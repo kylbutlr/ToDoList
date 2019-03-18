3b4c2dc7bf82891140644de6a802f26d4c748bbc
Kyle Butler <kylbutlr@gmail.com>
2019-03-18 09:11:55 -0700

    heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const client = new Client({
   user: 'rkrtjfhetbrpso',
   password: '6995b828e011276cc49322a5740b10da2a87c95ff2c5055eece0154e6b5e3835',
   database: 'dars6i8a5faoqk',
-  port: '5432',
+  port: 5432,
   host: 'ec2-23-23-241-119.compute-1.amazonaws.com',
   ssl: true
 });
